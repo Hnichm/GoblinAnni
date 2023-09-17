d53862355afa491a49109f069ea4fc95d6fbdfe6
Hnichm <125336099+Hnichm@users.noreply.github.com>
2023-09-16 21:20:44 -0500

    Updated health

--- dist/Entities/goblinBloodletter.js
+++ dist/Entities/goblinBloodletter.js
@@ -2,7 +2,7 @@ import Enemy from "./enemy.js";
 
 export default class GoblinBloodletter extends Enemy {
   constructor() {
-    super(10, 20, "Goblin Bloodletter");
+    super(0, 20, "Goblin Bloodletter");
     // document.querySelector(".enemy-image-tag").src = TODO;
   }
 }
