1eb83d836081e8b0078c9728430d32d1d8dd3dbc
Hnichm <125336099+Hnichm@users.noreply.github.com>
2023-09-16 21:20:58 -0500

    Updated health to 30

--- dist/Entities/goblinBrute.js
+++ dist/Entities/goblinBrute.js
@@ -2,7 +2,7 @@ import Enemy from "./enemy.js";
 
 export default class GoblinBrute extends Enemy {
   constructor() {
-    super(10, 20, "Goblin Brute");
+    super(30, 20, "Goblin Brute");
     document.querySelector(".enemy-image-tag").src =
       "../assets/Red Goblin Punch.png";
   }
